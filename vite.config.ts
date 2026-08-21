38326a2ddedfb6a33a5ff24de6704d6c2b57b8c9
natelwhite <natelwhite01@gmail.com>
2026-08-20 20:16:06 -0400

    syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 				fallback: '404.html',
 				precompress: false,
 				strict: true
-			})
+			}),
 			paths: {
 				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 			}
