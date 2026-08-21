747846ae293a1fad2c50823cffba56f43ff29bfd
natelwhite <natelwhite01@gmail.com>
2026-08-20 20:55:38 -0400

    static build only, no jekyll

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = { 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
 			fallback: '404.html',
-			precompress: false,
-			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
