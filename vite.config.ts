d6b3a034e92675205648621b93c0ae454f3c7e24
natelwhite <natelwhite01@gmail.com>
2026-08-20 20:14:36 -0400

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,15 @@ export default defineConfig({
 				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 			adapter: adapter({
-				fallback: '200.html'
+				pages: 'build',
+				assets: 'build',
+				fallback: '404.html',
+				precompress: false,
+				strict: true
 			})
+			paths: {
+				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			}
 		})
 	]
 });
