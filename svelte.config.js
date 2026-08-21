1b5889474e6e0fab0e5da3b5b7bd36d30b9702d4
natelwhite <natelwhite01@gmail.com>
2026-08-20 21:22:53 -0400

    idk

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = { 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true,
-		}),
+		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
 		}
 	}
 };
