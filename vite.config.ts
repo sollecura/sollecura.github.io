1ea391ba09580723c602507d9a1ac43b5c2af83d
natelwhite <natelwhite01@gmail.com>
2026-08-20 20:40:18 -0400

    fixed?

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter({
-				pages: 'build',
-				assets: 'build',
-				fallback: '404.html',
-				precompress: false,
-				strict: true
-			}),
-			paths: {
-				base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			}
-		})
-	]
+	plugins: [sveltekit()]
 });
